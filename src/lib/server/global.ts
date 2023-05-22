d79c78b502786bbb79c4d41c55352298025028a8
Gxorge <github@ghott.me>
2023-05-22 21:00:50 +0100

    quick change to db conn
    hopefully further reduces the risk of sql injection

--- src/lib/server/global.ts
+++ src/lib/server/global.ts
@@ -9,7 +9,8 @@ export const dbPool = mariadb.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    multipleStatements: false
 });
 const inputAllowed = /^[a-zA-Z0-9_]+$/;
 const tokenSecret = process.env.TOKEN_SECRET;
